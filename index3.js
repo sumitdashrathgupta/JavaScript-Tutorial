19981d8e6da3e789572b779fea5e4c5c265fb0cd
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2024-06-18 19:12:13 +0530

    index3.js add file

---
+++ index3.js
@@ -0,0 +1,24 @@
+// function word(a) {
+//     return a.split('').sort().join('');
+// }
+
+
+// let sys=word('webmaster')
+// console.log(sys)
+
+const persons = ['sumit', 'raj', 'vipin', 'roshan', 'ajay', 'nagu'];
+// console.log(persons)
+// for(let value in persons){
+//     console.log(value)
+// }
+// let ok =persons.forEach((Element,index)=>{
+//     return Element,index;
+// })
+
+// console.log(ok)
+// persons.pop('Prince')
+// console.log(persons)
+
+// console.log(persons.includes('')) 
+// console.log(persons.indexOf('')) 
+console.log(persons.includes('')) 
