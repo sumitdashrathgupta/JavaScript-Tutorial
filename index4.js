ba65e4a1d7b3e1285c02cf36596147947b431d41
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2024-06-19 15:15:03 +0530

    Javascript day task

---
+++ index4.js
@@ -0,0 +1,14 @@
+let text="sumit gupta"
+let cat=Array.from(text)
+// console.log(cat)
+
+// let elemap=cat.map((elemap,index)=>{
+//     console.log(elemap,"-",index)
+// })
+
+// console.log(text.indexOf("g"))
+// console.log(text.includes("o"))
+
+// console.log(text.search(/u/i))
+// console.log(text.match("o"))
+// console.log(text.matchAll("g"))
