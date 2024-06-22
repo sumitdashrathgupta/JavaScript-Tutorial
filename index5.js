0101de1a958a242f2051989a8232fcb261331894
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2024-06-22 21:01:26 +0530

    Javascript day task

---
+++ index5.js
@@ -0,0 +1,49 @@
+// const sum = (x, y) => {
+//     return x + y;
+// }
+
+// const data=sum(12,4)
+// console.log(data)
+
+
+// String
+
+let name1 = "sumit gupta"
+// console.log(name1.length)
+// console.log(`My name is ${name1}`)
+// console.log(name1.toUpperCase())
+// console.log(name1.toLowerCase())
+// console.log(name1.length)
+// console.log(name1.trim())
+// for(let i=0;i<name1.length;i++){
+//     console.log(i)
+// }
+
+
+//Pratice
+
+// 1)
+// console.log("sumit\"".length)
+
+
+// 2)
+// console.log(
+//     name1.includes("sumit")
+// )
+
+
+//3)
+
+
+// function sys() {
+//     return word.toUpperCase();
+// }
+
+// let word = prompt("Enter the any word than conver to uppercase")
+// let data = sys(word);
+
+// alert(`Your word Upper Case ${data}`)
+
+
+//4)
+// console.log(name1.replace("gupta","Developer"))
