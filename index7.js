3ab871388b035ba29f56603de7193464d8eefba8
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2024-07-03 20:32:34 +0530

    upload

--- index7.js
+++ index7.js
@@ -4,5 +4,20 @@
 // console.log(define1)
 // const define1=new Date(2024,2,23)
 // console.log(define1)
-const define1=new Date(2024,2,23,20)
-console.log(define1)
+// const define1=new Date(2024,2,23,20)
+// console.log(define1)
+// const sys=new Date()
+// console.log(sys)
+
+// const system="2024-07-03T07:47:33.866Z"
+// const ok=new Date(system)
+// console.log(ok)
+
+// const date=new Date(2025,3,23)
+// console.log(date)
+
+const date=new Date()
+// const sys=date.toLocaleString()
+// const sys=date.toLocaleDateString()
+const sys=date.toLocaleTimeString()
+console.log(sys)
